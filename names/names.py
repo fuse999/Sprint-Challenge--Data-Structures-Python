044fad1f57dbf91876e752e2cfb48403f81836a4
riley pence <rileypence4@gmail.com>
2020-03-27 13:26:06 -0700

    New MVP
    Updated names to include a time for each method and updated reverse to use recursion

--- names/names.py
+++ names/names.py
@@ -1,8 +1,6 @@
 import time
 from binary_search_tree import BinarySearchTree
 
-start_time = time.time()
-
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -15,10 +13,20 @@ duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
 
-# duplicates = set(names_1) & set(names_2)
+# Orignal O(n^2)
+start_time = time.time()
 
-# no set option 
-# with BST
+for name_1 in names_1:
+    for name_2 in names_2:
+        if name_1 == name_2:
+            duplicates.append(name_1)
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
+
+# with BST O(n log n)
+start_time = time.time()
 
 bst = BinarySearchTree("")
 for name_2 in names_2: 
@@ -36,3 +44,10 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+start_time = time.time()
+
+duplicates = set(names_1) & set(names_2)
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
